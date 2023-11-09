60f2c2e5274b20ba8e8a59106a750499ffaede15
Erin Santosa <tun35949@temple.edu>
2023-11-09 11:40:54 -0500

    created serviceConnection & override the functions to have timerBinder = service as TimerService.TimerBinder

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,11 +1,27 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var timerBinder: TimerService.TimerBinder
 
+    val serviceConnection = object : ServiceConnection{
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+        }
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
