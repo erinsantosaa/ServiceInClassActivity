2c2b38c840f89eb653d6c68894e3da064fc7ac6b
Erin Santosa <tun35949@temple.edu>
2023-11-15 15:49:05 -0500

    added job & scope, removed TimeBinder inner class

--- app/src/main/java/edu/temple/myapplication/TimerService.kt
+++ app/src/main/java/edu/temple/myapplication/TimerService.kt
@@ -2,124 +2,34 @@ package edu.temple.myapplication
 
 import android.app.Service
 import android.content.Intent
-import android.os.Binder
-import android.os.Handler
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.*
 
-@Suppress("ControlFlowWithEmptyBody")
 class TimerService : Service() {
 
-    lateinit var timerHandler: Handler
+    private var countdownValue: Int = 0
+    private var isRunning: Boolean = false
 
-    private var isRunning = false
+    private val job = SupervisorJob()
+    private val scope = CoroutineScope(Dispatchers.Default + job)
 
-    lateinit var t: TimerThread
-
-    private var paused = false
-
-    inner class TimerBinder : Binder() {
-
-        // Check if Timer is already running
-        var isRunning: Boolean
-            get() = this@TimerService.isRunning
-            set(value) {this@TimerService.isRunning = value}
-
-        // Start a new timer
-        fun start(startValue: Int){
-
-            if (!paused) {
-                if (!isRunning) {
-                    if (::t.isInitialized) t.interrupt()
-                    this@TimerService.start(startValue)
-                }
-            } else {
-                pause()
-            }
-        }
-
-        // Stop a currently running timer
-        fun stop() {
-            if (::t.isInitialized || isRunning) {
-                t.interrupt()
-            }
-        }
-
-        // Pause a running timer
-        fun pause() {
-            this@TimerService.pause()
-        }
-
-        fun getService() : TimerService {
-            return this@TimerService
-        }
-
-        fun setHandler(handler: Handler) {
-            timerHandler = handler
-        }
-
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-
-        Log.d("TimerService status", "Created")
-    }
-
-    override fun onBind(intent: Intent): IBinder {
-        return TimerBinder()
-    }
-
-    fun start(startValue: Int) {
-        t = TimerThread(startValue)
-        t.start()
-    }
-
-    fun pause () {
-        if (::t.isInitialized) {
-            paused = !paused
-            isRunning = !paused
-        }
-    }
-
-    inner class TimerThread(private val startValue: Int) : Thread() {
-
-        override fun run() {
+    private fun startCountdown() {
+        if (!isRunning) {
             isRunning = true
-            try {
-                for (i in startValue downTo 1)  {
+            scope.launch {
+                for (i in countdownValue downTo 1) {
                     Log.d("Countdown", i.toString())
-
-                        while (paused);
-                        if(::timerHandler.isInitialized){
-                            timerHandler.sendEmptyMessage(i)
-                        }
-                        sleep(1000)
-
+                    delay(1000)
                 }
-                isRunning = false
-            } catch (e: InterruptedException) {
-                Log.d("Timer interrupted", e.toString())
-                isRunning = false
-                paused = false
+                stopSelf()
             }
         }
-
-    }
-
-    override fun onUnbind(intent: Intent?): Boolean {
-        if (::t.isInitialized) {
-            t.interrupt()
-        }
-
-        return super.onUnbind(intent)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
-        Log.d("TimerService status", "Destroyed")
+        job.cancel()
     }
-
 
 }
